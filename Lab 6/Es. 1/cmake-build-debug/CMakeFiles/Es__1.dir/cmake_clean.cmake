2269b41a8b0eba2437ca02fe7cc68a5ee6fdb9be
Guglielmo Borzone <borzoneg@gmail.com>
2019-12-01 11:50:20 +0100

    Add existing files

--- Lab 6/Es. 1/cmake-build-debug/CMakeFiles/Es__1.dir/cmake_clean.cmake
+++ Lab 6/Es. 1/cmake-build-debug/CMakeFiles/Es__1.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Es__1.dir/main.c.o"
-  "Es__1"
+  "CMakeFiles/Es__1.dir/main.c.obj"
+  "Es__1.exe"
+  "Es__1.exe.manifest"
   "Es__1.pdb"
+  "libEs__1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
